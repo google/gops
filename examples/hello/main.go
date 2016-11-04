eab05457139e2394807ba91389fe1b47a8cacf42
Jaana Burcu Dogan <jbd@google.com>
2016-11-03 22:42:22 -0700

    add example

---
+++ examples/hello/main.go
@@ -0,0 +1,15 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"time"
+
+	_ "hello/gops/agent"
+)
+
+func main() {
+	time.Sleep(time.Hour)
+}
