467d918128028e0b795ae7377bbe7b2fb1fbe602
Jaana Burcu Dogan <jbd@google.com>
2017-05-10 14:38:11 -0700

    examples/hello: simplify the minimal example

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -12,9 +12,7 @@ import (
 )
 
 func main() {
-	if err := agent.Listen(&agent.Options{
-		Addr: "127.0.0.1:4321",
-	}); err != nil {
+	if err := agent.Listen(nil); err != nil {
 		log.Fatal(err)
 	}
 	time.Sleep(time.Hour)
