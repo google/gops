1205df8eaa68f999dabe765667f04e6cbdd89976
kumakichi <xyesan@gmail.com>
2020-08-05 09:01:02 +0800

    Drop test for EMFILE

--- goprocess/gp_test.go
+++ goprocess/gp_test.go
@@ -1,41 +1,9 @@
 package goprocess
 
-import (
-	"os"
-	"sync"
-	"syscall"
-	"testing"
-
-	"github.com/keybase/go-ps"
-)
+import "testing"
 
 func BenchmarkFindAll(b *testing.B) {
 	for ii := 0; ii < b.N; ii++ {
 		_ = FindAll()
 	}
-}
-
-func TestEMFILE(t *testing.T) {
-	pss, err := ps.Processes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(pss))
-
-	for _, pr := range pss {
-		pr := pr
-		go func() {
-			defer wg.Done()
-			_, _, _, _, err := isGo(pr)
-			if err != nil {
-				if e, ok := err.(*os.PathError); ok && e.Err == syscall.EMFILE {
-					t.Errorf("pid:%d got EMFILE error", pr.Pid())
-				}
-			}
-		}()
-	}
-
-	wg.Wait()
 }
