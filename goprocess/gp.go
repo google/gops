38fcd2c520027dcbdd5ed501c1e2efdd8385223e
Jaroslavs Samcuks <yarcat@gmail.com>
2020-09-05 20:00:07 +0200

    main loop moved to a go-routine

--- goprocess/gp.go
+++ goprocess/gp.go
@@ -33,38 +33,42 @@ func FindAll() []P {
 		return nil
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(len(pss))
 	found := make(chan P)
-	limitCh := make(chan struct{}, concurrencyProcesses)
 
-	for _, pr := range pss {
-		limitCh <- struct{}{}
-		pr := pr
-		go func() {
-			defer func() { <-limitCh }()
-			defer wg.Done()
-
-			path, version, agent, ok, err := isGo(pr)
-			if err != nil {
-				// TODO(jbd): Return a list of errors.
-			}
-			if !ok {
-				return
-			}
-			found <- P{
-				PID:          pr.Pid(),
-				PPID:         pr.PPid(),
-				Exec:         pr.Executable(),
-				Path:         path,
-				BuildVersion: version,
-				Agent:        agent,
-			}
-		}()
-	}
 	go func() {
-		wg.Wait()
-		close(found)
+		var wg sync.WaitGroup
+		wg.Add(len(pss))
+
+		defer func() {
+			wg.Wait()
+			close(found)
+		}()
+
+		limitCh := make(chan struct{}, concurrencyProcesses)
+		for _, pr := range pss {
+			limitCh <- struct{}{}
+			pr := pr
+			go func() {
+				defer func() { <-limitCh }()
+				defer wg.Done()
+
+				path, version, agent, ok, err := isGo(pr)
+				if err != nil {
+					// TODO(jbd): Return a list of errors.
+				}
+				if !ok {
+					return
+				}
+				found <- P{
+					PID:          pr.Pid(),
+					PPID:         pr.PPid(),
+					Exec:         pr.Executable(),
+					Path:         path,
+					BuildVersion: version,
+					Agent:        agent,
+				}
+			}()
+		}
 	}()
 	var results []P
 	for p := range found {
