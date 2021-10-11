6124f2eb47011901d174c91814c8e6201f4dd5b2
Tobias Klauser <tklauser@distanz.ch>
2021-10-11 13:39:58 +0200

    agent: fix solaris build
    Solaris doesn't provide the SO_REUSEPORT socket option.

--- agent/sockopt_unsupported.go
+++ agent/sockopt_unsupported.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (js && wasm) || plan9 || windows
-// +build js,wasm plan9 windows
+//go:build (js && wasm) || plan9 || solaris || windows
+// +build js,wasm plan9 solaris windows
 
 package agent
 
