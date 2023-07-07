25b77ae0c9ad2922bf45b22146b052f98274c34d
Qishuai Liu <lqs@lqs.me>
2023-07-07 17:44:36 +0900

    fix return value in memory_limit_lt1.19.go

--- agent/memory_limit_lt1.19.go
+++ agent/memory_limit_lt1.19.go
@@ -14,7 +14,7 @@ import (
 
 func setMemoryLimit(limit int64) (int64, error) {
 	if limit < 0 {
-		return math.MaxInt64
+		return math.MaxInt64, nil
 	}
 	return 0, errors.New("memory limit not supported on Go versions before 1.19")
 }
