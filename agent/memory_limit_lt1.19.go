c8c413fa57246b64e9aa0dc2aacbc2a5fa70bcb5
Qishuai Liu <lqs@lqs.me>
2023-07-07 13:18:14 +0900

    add "setmemlimit" command for utilizing new memory limit feature in Go 1.19+

---
+++ agent/memory_limit_lt1.19.go
@@ -0,0 +1,20 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !go1.19
+// +build !go1.19
+
+package agent
+
+import (
+	"errors"
+	"math"
+)
+
+func setMemoryLimit(limit int64) (int64, error) {
+	if limit < 0 {
+		return math.MaxInt64
+	}
+	return 0, errors.New("memory limit not supported on Go versions before 1.19")
+}
