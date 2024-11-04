google/gops

commit 390a41310ac15b48471b7c7a0463e670ed581237

Report more Stack info, as stack-mspan-inuse、stack-mspan-sys etc. (#63)

agent/agent.go
@@ -200,13 +200,20 @@ func handle(conn io.ReadWriter, msg []byte) error {
 		fmt.Fprintf(conn, "heap-objects: %v\n", s.HeapObjects)
 		fmt.Fprintf(conn, "stack-in-use: %v\n", formatBytes(s.StackInuse))
 		fmt.Fprintf(conn, "stack-sys: %v\n", formatBytes(s.StackSys))
+		fmt.Fprintf(conn, "stack-mspan-inuse: %v\n", formatBytes(s.MSpanInuse))
+		fmt.Fprintf(conn, "stack-mspan-sys: %v\n", formatBytes(s.MSpanSys))
+		fmt.Fprintf(conn, "stack-mcache-inuse: %v\n", formatBytes(s.MCacheInuse))
+		fmt.Fprintf(conn, "stack-mcache-sys: %v\n", formatBytes(s.MCacheSys))
+		fmt.Fprintf(conn, "other-sys: %v\n", formatBytes(s.OtherSys))
+		fmt.Fprintf(conn, "gc-sys: %v\n", formatBytes(s.GCSys))
 		fmt.Fprintf(conn, "next-gc: when heap-alloc >= %v\n", formatBytes(s.NextGC))
 		lastGC := "-"
 		if s.LastGC != 0 {
 			lastGC = fmt.Sprint(time.Unix(0, int64(s.LastGC)))
 		}
 		fmt.Fprintf(conn, "last-gc: %v\n", lastGC)
-		fmt.Fprintf(conn, "gc-pause: %v\n", time.Duration(s.PauseTotalNs))
+		fmt.Fprintf(conn, "gc-pause-total: %v\n", time.Duration(s.PauseTotalNs))
+		fmt.Fprintf(conn, "gc-pause: %v\n", s.PauseNs[(s.NumGC+255)%256])
 		fmt.Fprintf(conn, "num-gc: %v\n", s.NumGC)
 		fmt.Fprintf(conn, "enable-gc: %v\n", s.EnableGC)
 		fmt.Fprintf(conn, "debug-gc: %v\n", s.DebugGC)

commit c6afcdffb44887f528c2b808c1741da1a19638bd

agent: fix mistakes in off-heap mstat naming
Some statistics were mislabeled as related to the stack while those are
off-heap memory statistics. I'm guessing this is due to their proximity
with the stack stats.

Here is the general documentation for these metrics:
https://github.com/golang/go/blob/go1.23.2/src/runtime/mstats.go#L209-L218

	// Off-heap memory statistics.
	//
	// The following statistics measure runtime-internal
	// structures that are not allocated from heap memory (usually
	// because they are part of implementing the heap). Unlike
	// heap or stack memory, any memory allocated to these
	// structures is dedicated to these structures.
	//
	// These are primarily useful for debugging runtime memory
	// overheads.>

Fixes: 390a41310ac1 ("Report more Stack info, as stack-mspan-inuse stack-mspan-sys etc. (#63)")

Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

agent/agent.go
@@ -229,10 +229,10 @@ func handle(conn io.ReadWriter, msg []byte) error {
 		fmt.Fprintf(conn, "heap-objects: %v\n", s.HeapObjects)
 		fmt.Fprintf(conn, "stack-in-use: %v\n", formatBytes(s.StackInuse))
 		fmt.Fprintf(conn, "stack-sys: %v\n", formatBytes(s.StackSys))
-		fmt.Fprintf(conn, "stack-mspan-inuse: %v\n", formatBytes(s.MSpanInuse))
-		fmt.Fprintf(conn, "stack-mspan-sys: %v\n", formatBytes(s.MSpanSys))
-		fmt.Fprintf(conn, "stack-mcache-inuse: %v\n", formatBytes(s.MCacheInuse))
-		fmt.Fprintf(conn, "stack-mcache-sys: %v\n", formatBytes(s.MCacheSys))
+		fmt.Fprintf(conn, "mspan-inuse: %v\n", formatBytes(s.MSpanInuse))
+		fmt.Fprintf(conn, "mspan-sys: %v\n", formatBytes(s.MSpanSys))
+		fmt.Fprintf(conn, "mcache-inuse: %v\n", formatBytes(s.MCacheInuse))
+		fmt.Fprintf(conn, "mcache-sys: %v\n", formatBytes(s.MCacheSys))
 		fmt.Fprintf(conn, "other-sys: %v\n", formatBytes(s.OtherSys))
 		fmt.Fprintf(conn, "gc-sys: %v\n", formatBytes(s.GCSys))
 		fmt.Fprintf(conn, "next-gc: when heap-alloc >= %v\n", formatBytes(s.NextGC))